aedd0f671ef443585c45841924859a932bec05cb
Yuan_Feng <y.feng-28@sms.ed.ac.uk>
2020-02-22 15:40:21 +0000

    add the fuzzy match

---
+++ fuzzysearch.py
@@ -0,0 +1,36 @@
+#pip install pybktree
+#pip install fuzzywuzzy
+import collections
+import json
+import pybktree_mo
+import time
+
+
+
+class fuzzy():
+    def __init__(self):
+        with open('words_dictionary.json','r') as f:
+            self.dictionary = json.load(f).keys()
+            self.tree = pybktree_mo.BKTree(pybktree_mo.hamming_distance, self.dictionary)
+    
+    def bktreeSearch(self, query):
+        if len(query) <= 3:
+            limit = 1
+        else: 
+            limit = 2
+        return sorted(self.tree.find(query, limit))
+        
+    
+    
+if __name__ == "__main__":
+    time_1 = time.time()
+    k = fuzzy()
+    time_2 = time.time()
+    print("indexing time: ",time_2 - time_1)
+    k.bktreeSearch('motherfucker')
+    time_3 = time.time()
+    print("Searching time:", time_3 - time_2)
+    
+    
+        
+        
